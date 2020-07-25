223a35d7ae6c26d529544950d92634ed65688c27
Taiki Sekiguchi <ukurere3617@gmai.com>
2020-07-25 23:56:24 +0900

    add ml5

--- sketch.js
+++ sketch.js
@@ -61,8 +61,31 @@ var button4;
 var inp;
 var myId;
 
+let classifier;
+// Model URL
+let imageModelURL = 'https://teachablemachine.withgoogle.com/models/UQrFu3ScX/model.json';
+
+// Video
+let video;
+let flippedVideo;
+// To store the classification
+let label = "";
+
+// Load the model first
+function preload() {
+  classifier = ml5.imageClassifier(imageModelURL);
+}
+
 function setup() {
-  createCanvas(640,480);
+  createCanvas(320, 260);
+  // Create the video
+  video = createCapture(VIDEO);
+  video.size(320, 240);
+  video.hide();
+
+  flippedVideo = ml5.flipImage(video)
+  // Start classifying
+  classifyVideo();
 
   
 
@@ -182,7 +205,25 @@ function addVideo(call){
 
 function draw() { 
   background(0);
-  ellipse(w/2,h/2,100,100);
+  // Draw the video
+  image(flippedVideo, 0, 0);
+
+  // Draw the label
+  fill(255);
+  textSize(16);
+  textAlign(CENTER);
+  text(label, width / 2, height - 4);
+
+  if(label == "whispering"){
+    localStream.getAudioTracks()[0].enabled = true;
+  }else if(label == "Listening"){
+    let video = document.getElementById('their-video');
+    video.muted = true;
+  }else if(label == "None"){
+    localStream.getAudioTracks()[0].enabled = false;
+    let video = document.getElementById('their-video');
+    video.muted = false;
+  }
 }
 
 function changeBG() {
@@ -201,4 +242,24 @@ function changeBG3() {
 
 function changeBG4() {
   localStream.getAudioTracks()[0].enabled = true;
+}
+
+// Get a prediction for the current video frame
+function classifyVideo() {
+  flippedVideo = ml5.flipImage(video)
+  classifier.classify(flippedVideo, gotResult);
+}
+
+// When we get a result
+function gotResult(error, results) {
+  // If there is an error
+  if (error) {
+    console.error(error);
+    return;
+  }
+  // The results are in an array ordered by confidence.
+  // console.log(results[0]);
+  label = results[0].label;
+  // Classifiy again!
+  classifyVideo();
 }
